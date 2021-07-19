0d9b6e0fea598dac1cdf1d8a9bb249f904cade6c
neilscallywag <64523806+neilscallywag@users.noreply.github.com>
2021-07-20 01:12:45 +0800

    Update iprb.py
    Updated Algorithm as previous code assumes replacement in population. This code assumes without replacement. Hence the answer tends towards the given sample.

--- iprb.py
+++ iprb.py
@@ -7,7 +7,20 @@ Return: The probability that two randomly selected mating organisms will produce
 
 import random as random
 
-def helper(k,m,n):
+def parent(k,m,n):
+    i = random.uniform(0, 1)
+    if i <= (k/(k+m+n)):
+        Y = ['A','A']
+        k -= 1
+    elif (k/(k+m+n)) <= i <= (1 - (n/(k+m+n))):
+        Y = ['A','a']
+        m -= 1
+    else:
+        Y = ['a','a']
+        n -= 1
+    return f1(k,m,n,Y)
+
+def f1(k,m,n,Y):
     i = random.uniform(0, 1)
     if i <= (k/(k+m+n)):
         X = ['A','A']
@@ -15,20 +28,16 @@ def helper(k,m,n):
         X = ['A','a']      
     else:
         X = ['a','a']
-    return X
+    return list(random.choice(X)) + list(random.choice(Y))
 
-def newF1(k,m,n):   
-    o = list(random.choice(helper(k,m,n)))
-    p = list(random.choice(helper(k,m,n)))
-    f1 = o + p
-    return f1
+
     
 def prob(k,m,n):
    counter = 0
-   r = 999999
+   r = 1000
    for i in range(r):
-       q = newF1(k,m,n)
-       if ('A' in q ): 
+       q = parent(k,m,n)
+       if ('A' in q ):
            counter += 1          
        else:
            counter +=0         
